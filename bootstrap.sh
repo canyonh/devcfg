2cc81fd08ba5f8c830b1250359f9f27f538baf46
kxhuan <kxhuan@kxhuan.com>
2022-08-27 22:06:23 +1000

    change keybind of telescope; add apt-install.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,1 @@
-# utility
-sudo apt install -y git python3 python3-pip openssh-server corkscrew ripgrep
-
-# nvim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install neovim
-
-# build tools and nvim support
-sudo apt install build-essential gcc g++ cmake clang libclang-dev nodejs
-pip install pynvim
-npm -i neovim
+. ./bootstrap.sh
