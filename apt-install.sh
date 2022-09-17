30255c0d5ef80a477e1368f61c0fc384775038cd
kxhuan <kxhuan@dolby.com>
2022-09-17 15:35:56 +1000

    fix pipenv install

--- apt-install.sh
+++ apt-install.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 apt update
 
 # utility
-sudo apt install -y git python-is-python3 python3 python3-pip python3-venv openssh-server corkscrew ripgrep fd-find pipenv cppcheck
+sudo apt install -y git python-is-python3 python3 python3-pip python3-venv openssh-server corkscrew ripgrep fd-find cppcheck
 
 # nvim
 sudo apt install -y neovim
@@ -21,3 +21,5 @@ npm i neovim
 # install black
 pip install black==22.6.0
 
+# install pipenv via python. apt has wrong info
+pip install pipenv
