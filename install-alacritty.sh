f4c66af3ead31dfc6a851ea776344e2fe5a93880
kxhuan <kxhuan@dolby.com>
2022-09-24 12:56:37 +1000

    add script to install alacritty

---
+++ install-alacritty.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:aslatter/ppa
+sudo apt update
+sudo apt install alacritty
+# sudo apt remove --auto-remove alacritty
+# sudo add-apt-repository --remove ppa:aslatter/ppa
+ln -s $HOME/devcfg/.config/alacritty $/HOME/.config/alacritty
