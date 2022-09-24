1437b519f33ce58dd076a09330fa76ff6c6f8e29
kxhuan <kxhuan@dolby.com>
2022-09-24 13:00:43 +1000

    comments to update shortcut on linux mint

--- install-alacritty.sh
+++ install-alacritty.sh
@@ -1,6 +1,13 @@
 sudo add-apt-repository ppa:aslatter/ppa
 sudo apt update
 sudo apt install alacritty
+ln -s $HOME/devcfg/.config/alacritty $/HOME/.config/alacritty
+
+# to remove
 # sudo apt remove --auto-remove alacritty
 # sudo add-apt-repository --remove ppa:aslatter/ppa
-ln -s $HOME/devcfg/.config/alacritty $/HOME/.config/alacritty
+
+# to setup short cut ctrl-alt-t to open alacritty:
+#  * Open keyboard -> shortcuts
+#  * Remove launch terminal shortcut in launchers
+#  * Add custom short cut ctrl-alt-t to alacritty
