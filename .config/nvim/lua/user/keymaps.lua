35133b06a65012d85ee6964f0d4bc6048c5e1301
kxhuan <kxhuan@dolby.com>
2022-09-29 20:41:00 +1000

    easy movement when using termdebug

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -19,14 +19,14 @@ local keymap = vim.api.nvim_set_keymap
 --   command_mode = "c",
 
 -- Normal --
+-- Not useful? perhaps covered by tmux?
 -- Better window navigation
 --keymap("n", "<C-h>", "<C-w>h", opts)
 --keymap("n", "<C-j>", "<C-w>j", opts)
 --keymap("n", "<C-k>", "<C-w>k", opts)
 --keymap("n", "<C-l>", "<C-w>l", opts)
 
--- replaced by nvim-tree.lua
--- keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+-- nvim-tree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Resize with arrows
@@ -53,26 +53,30 @@ keymap("n", "<S-h>", ":bprevious<CR>", opts)
 -- keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+-- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+-- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+
+-- @todo is it useful?
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
 -- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 -- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+-- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
--- Better terminal navigation
-
--- disabled for fzf integration
--- keymap("t", "<esc>", "<C-\\><C-N>", term_opts)
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- Better terminal navigation for termdebug 
+function _G.set_termdebug_keymaps()
+  keymap("t", "<esc>", "<C-\\><C-N>", term_opts)
+  keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+  keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+  keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+  keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+end
+vim.cmd('autocmd! TermOpen term://*gdb* lua set_termdebug_keymaps()')
 
 -- Telescope (too slow)
 -- keymap("n", "<c-p>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
@@ -86,9 +90,9 @@ keymap("n", "<c-n>", "<cmd>lua require('fzf-lua').live_grep()<cr>", opts)
 keymap("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<cr>", opts)
 
 -- vcs files, commits branches
-keymap("n", "<leader>vf","<cmd>lua require('fzf-lua').git_files()<cr>", opts)
-keymap("n", "<leader>vc","<cmd>lua require('fzf-lua').git_commits()<cr>", opts)
-keymap("n", "<leader>vb","<cmd>lua require('fzf-lua').git_branches()<cr>", opts)
+keymap("n", "<leader>vf", "<cmd>lua require('fzf-lua').git_files()<cr>", opts)
+keymap("n", "<leader>vc", "<cmd>lua require('fzf-lua').git_commits()<cr>", opts)
+keymap("n", "<leader>vb", "<cmd>lua require('fzf-lua').git_branches()<cr>", opts)
 
 -- grep live, grep buffer
 keymap("n", "<leader>gl", "<cmd>lua require('fzf-lua').live_grep()<cr>", opts)
