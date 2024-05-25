a4fe626a1f4b2b1ea1c2397ac98800e52a200595
Kun-Yao Huang <canyonh@gmail.com>
2024-05-25 13:45:41 +1000

    update C++ standard; update email/name

--- setup-git.sh
+++ setup-git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-git config --global user.email "kxhuan@dolby.com"
-git config --global user.name "kxhuan"
+git config --global user.email "canyonh@gmail.com"
+git config --global user.name "Kun-Yao Huang"
 git config --global diff.tool nvimdiff
 git config --global merge.tool nvimdiff
