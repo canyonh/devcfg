275146f2b194159bbf3993999f1dc5a5dc021ec5
kxhuan <canyonh@gmail.com>
2024-08-09 20:49:34 +1000

    fix pynvim python package again

--- apt-install.sh
+++ apt-install.sh
@@ -5,7 +5,7 @@ sudo apt update -y
 sudo apt install curl
 sudo sh -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -"
 sudo add-apt-repository ppa:neovim-ppa/unstable
-apt update
+apt update -y
 
 # utility
 sudo apt install -y git python-is-python3 python3 python3-pip python3-venv software-properties-common openssh-server corkscrew ripgrep fd-find cppcheck fzf zsh
@@ -18,11 +18,10 @@ sudo apt install -y tmux
 
 # build tools and nvim support
 sudo apt install -y build-essential gcc g++ cmake clang libclang-dev nodejs mold
-pip install --user pynvim
+
+#pip install --user pynvim
+sudo apt install -y python3-pynvim
 npm i neovim
 
 # install black
-pip install --user black
-
-# install pipenv via python. apt has wrong version which does not work with the others
-pip install --user pipenv
+#pip install --user black
