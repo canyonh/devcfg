41ea41d62c6b9bc63b6d0323a48904f272a14a02
Kun-Yao Huang <khuang@anduril.com>
2025-09-12 09:55:36 +1000

    Update plugin; improve diagnostic

--- .config/nvim/lua/kxhuan/core/keymaps.lua
+++ .config/nvim/lua/kxhuan/core/keymaps.lua
@@ -22,7 +22,14 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current tab in a new tab" })
 keymap.set("n", "gd", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+
+-- Without telescope it seems to be faster
+-- keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references"})
+
 keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
 keymap.set("n", "ga", vim.lsp.buf.code_action, { desc = "Go to implementation" })
 keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Go to implementation" })
+
+-- TODO(khuang) make range format work
+-- keymap.set("v", "<leader>gf", vim.lsp.buf.format, { desc = "Range formatting" })
--- .config/nvim/lua/kxhuan/core/options.lua
+++ .config/nvim/lua/kxhuan/core/options.lua
@@ -36,9 +36,55 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 -- lsp options
+-- vim.diagnostic.config({
+--     virtual_text = true,
+--     virtual_lines = { current_line = true },
+--     --underline = true,
+--     --update_in_insert = false
+-- })
+
+--local function jumpWithVirtLineDiags(jumpCount)
+--	pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags") -- prevent autocmd for repeated jumps
+--
+--	vim.diagnostic.jump { count = jumpCount }
+--
+--	local initialVirtTextConf = vim.diagnostic.config().virtual_text
+--	vim.diagnostic.config {
+--		virtual_text = false,
+--		virtual_lines = { current_line = true },
+--	}
+--
+--	vim.defer_fn(function() -- deferred to not trigger by jump itself
+--		vim.api.nvim_create_autocmd("CursorMoved", {
+--			desc = "User(once): Reset diagnostics virtual lines",
+--			once = true,
+--			group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
+--			callback = function()
+--				vim.diagnostic.config { virtual_lines = false, virtual_text = initialVirtTextConf }
+--			end,
+--		})
+--	end, 1)
+--end
+--
+--vim.keymap.set("n", "ge", function() jumpWithVirtLineDiags(1) end, { desc = "󰒕 Next diagnostic" })
+--vim.keymap.set("n", "gE", function() jumpWithVirtLineDiags(-1) end, { desc = "󰒕 Prev diagnostic" })
+
 vim.diagnostic.config({
     virtual_text = true,
-    virtual_lines = { current_line = true },
-    underline = true,
-    update_in_insert = false
+    -- virtual_lines = { current_line = true },
+    --underline = true,
+    --update_in_insert = false
 })
+
+vim.keymap.set('n', 'go', function()
+  vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = true})
+
+  vim.api.nvim_create_autocmd('CursorMoved', {
+    group = vim.api.nvim_create_augroup('line-diagnostics', { clear = true }),
+    callback = function()
+      vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+      return true
+    end,
+  })
+end)
+
